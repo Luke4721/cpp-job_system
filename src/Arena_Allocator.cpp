4704b33c80e8b4fd3ccff03bd5a076a64bd24248
Luke472 <jetart78@gmail.com>
2026-01-12 21:14:33 +0530

    Implemented a simple single job struct

--- src/Arena_Allocator.cpp
+++ src/Arena_Allocator.cpp
@@ -29,6 +29,11 @@ struct Arena
         offset = aligned_offset + size;
         return ptr;
     }
+    //Invariant : After reset(), All objects allocated from the arena are invalid, and the next allocation will start from the beginning of the buffer.
+    void reset()
+    {
+        offset = 0;
+    }
 
     ~Arena()
     {
@@ -43,23 +48,59 @@ private:
     }
 };
 
-template <typename T>
+template <typename T, typename... Args>
 
-T* arena_allocate(struct Arena& arena)
+T* arena_allocate(struct Arena& arena, Args&&... args)
 {
     void* mem = arena.allocate(sizeof(T), alignof(T));
     if (!mem)
         return nullptr;
-    return new (mem) T();
+    return new (mem) T(std::forward<Args>(args)...);
 }
-template <typename T, typename... Args>
+template <typename T>
 void arena_destroy(T* obj)
 {
     if (obj)
         obj->~T();
 }
+//Job Structure 
+struct Job
+{
+    void (*fn)(void*);
+    void* data;
+};
+struct SumJobData {
+    int* array;
+    size_t count;
+    int* result;
 
+    SumJobData(int* array, size_t count, int* result)
+        : array(array), count(count), result(result) {}
+};
+
+
+void sum_job(void* ptr)
+{
+    auto* data = static_cast<SumJobData*>(ptr);
+
+    int sum=0;
+
+    for(size_t i=0; i< data->count; ++i)
+    {
+        sum += data->array[i];
+    }
+    *data->result = sum;
+}
 int main()
 {
-    
+    Arena frameArena(1024);
+    int values [] = {1,2,3,4,5};
+    int result =0;
+    auto* payload = arena_allocate<SumJobData>(frameArena, values, 5, &result);
+    Job job;
+    job.fn = &sum_job;
+    job.data = payload;
+    job.fn(job.data);
+
+    frameArena.reset();
 }
